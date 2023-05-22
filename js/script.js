695ce4633b10dc3786c731a644cd90f098e3a310
Caroline Going <carolinegoing2@gmail.com>
2023-05-22 11:27:12 -0400

    Created function to capture and check input, then add to guessedLetters array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // ul where the guessed letters appear
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersList = document.querySelector(".guessed-letters");
 // "Guess!" button
 const guessButton = document.querySelector(".guess");
 // text input to guess a letter
-const input = document.querySelector(".letter");
+const letterInput = document.querySelector(".letter");
 // p where the word in progress appears
 const wordInProgress = document.querySelector(".word-in-progress");
 // p where remaining guesses will display
@@ -15,8 +15,10 @@ const feedbackMessage = document.querySelector(".message");
 // button prompting user to play again
 const playAgain = document.querySelector(".play-again");
 
-//test word
+// test word
 const word = "magnolia";
+// array to contain guessed letters
+const guessedLetters = [];
 
 
 // placeholder for chosen word's letters
@@ -31,10 +33,41 @@ const placeholder = function (word){
 
 placeholder(word);
 
+// --- GUESS BUTTON EVENT HANDLER --- //
 guessButton.addEventListener("click", function (e){
     e.preventDefault();
 
-    const guess = input.value;
-    console.log(guess);
-    input.value = "";
-})
+    feedbackMessage.innerText = "";
+
+    const guess = letterInput.value;
+    const goodGuess = guessValidation(guess);
+    
+    if (goodGuess){
+        makeGuess(guess);
+    }
+    letterInput.value = "";
+});
+
+const guessValidation = function (input){
+    const acceptedLetter = /[a-zA-Z]/
+
+    if (input.length === 0){
+        feedbackMessage.innerText = "Please enter a letter.";
+    } else if (input.length > 1){
+        feedbackMessage.innerText = "Please enter only one letter at a time.";
+    } else if (!input.match(acceptedLetter)){
+        feedbackMessage.innerText = "Please enter a letter from A to Z.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(guess) {
+
+    if (guessedLetters.includes(guess)){
+        feedbackMessage.innerText = "You have already guessed that letter, please try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
